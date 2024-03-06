5711f8a9a7599d24810b33c6e514a66d04d7ca67
Ivan Romanenko <ivromane10@mts.ru>
2024-03-06 13:34:02 +0300

    final commit

--- NextTrack/ContentView.swift
+++ NextTrack/ContentView.swift
@@ -8,29 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var buttonsCount = [UUID(), UUID()]
+    @State private var isAnimated = false
     
     var body: some View {
         Button {
-            buttonsCount.removeLast()
-            buttonsCount.insert(UUID(), at: 0)
+            withAnimation(
+                .spring(
+                    response: 0.5,
+                    dampingFraction: 0.5,
+                    blendDuration: 0.0
+                )
+            ) {
+                isAnimated = true
+            } completion: {
+                isAnimated = false
+            }
         } label: {
             HStack(spacing: -4) {
-                ForEach(buttonsCount, id: \.self) { _ in
+                if isAnimated {
                     Image(systemName: "play.fill")
-                        .font(.title)
                         .transition(
-                            .scale
-                                .animation(
-                                    .spring(
-                                        response: 0.5,
-                                        dampingFraction: 0.5,
-                                        blendDuration: 0.0
-                                    )
-                                )
+                            .scale(isAnimated ? 0 : 1, anchor: .leading)
+                            .combined(with: .opacity)
+                        )
+                }
+                
+                Image(systemName: "play.fill")
+                
+                if !isAnimated {
+                    Image(systemName: "play.fill")
+                        .transition(
+                            .scale(isAnimated ? 1 : 0, anchor: .trailing)
+                            .combined(with: .opacity)
                         )
                 }
             }
+            .imageScale(.large)
         }
     }
 }
